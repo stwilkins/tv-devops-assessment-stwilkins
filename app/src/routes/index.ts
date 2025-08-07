0758cea6ed36180536f752954cc2b571d9ccff30
Scott Wilkins <scottiewilkins@gmail.com>
2025-08-06 23:01:14 -0700

    more file updates

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -6,4 +6,11 @@ router.get('/', (_req, res) => {
   res.send('Hello from Express + TypeScript!');
 });
 
+// A health check endpoint was added to satisfy requirements of assessment
+router.get('/health', (_req, res) => {
+  res.status(200).({
+    status: 'Healthcheck is good'
+  });
+})
+
 export default router;
