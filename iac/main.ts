81cc6493a6167aa231d382332f72f75a96ad0ef0
Scott Wilkins <scottiewilkins@gmail.com>
2025-08-07 17:21:18 -0700

    more changes

--- iac/main.ts
+++ iac/main.ts
@@ -1,5 +1,18 @@
-import { Construct } from "constructs";
+// main.ts, which pulled information from lib/config.ts and constructs/
+// Setup app and provider, AWS
 import { App, TerraformStack } from "cdktf";
+import { Provider } from "@cdktf/provider-aws";
+
+// Setup environment variables with config.ts from lib
+import { extractVariables } from "./lib/config"
+
+// Setup constructors, how to pull info from AWS
+import { Construct } from "constructs";
+import { ECS } from "./constructs/ecs"
+import { ECR } from "./constructs/ecr"
+import { IAM } from "./constructs/iam"
+import { NETWORK } from "./constructs/network"
+
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
